5db61891848bcc7ed5499c3d35ff48e79da01dee
mithaq-abdelkarim-bzy <mithaq.abdelkarim@beazley.com>
2026-09-04 13:28:59 +0100

    d2

--- hyperexponential.rater.rex-main/source_files/6350_v1.32.1/algorithms/constant.py
+++
@@ -1,5 +0,0 @@
-MAX_OPTIONS = 3
-MAX_LAYERS = 6
-MAX_ADDITIONAL_REFERENCES = 6
-
-fire_rating_factors = ["occupancy", "construction", "pc_code", "sprinkler", "bi_waiting_period", "bi_indemnity_period", "cbi", "fire_size_discount", "mexican_fonden"]
--- hyperexponential.rater.rex-main/source_files/6350_v1.32.1/view/constants.js
+++
@@ -1,6 +0,0 @@
-function MAX_LAYERS() {
-  return 6
-}
-
-
-export { MAX_LAYERS };
