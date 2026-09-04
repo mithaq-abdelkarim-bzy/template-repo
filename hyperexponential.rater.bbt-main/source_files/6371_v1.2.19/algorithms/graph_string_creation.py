b60d29361431a2b5c0a03fba8bb340c4df0857f7
mithaq-abdelkarim-bzy <mithaq.abdelkarim@beazley.com>
2026-09-04 10:41:08 +0100

    d

--- hyperexponential.rater.bbt-main/source_files/6371_v1.2.19/algorithms/graph_string_creation.py
+++
@@ -1,9 +0,0 @@
-import hx
-
-def adjust_graph_strings(hxd):
-    chart_path = hxd.cds.rating_summary.chart_premium_breakdown
-    chart_path.technical.metric_string = "TPI"
-    chart_path.benchmark.metric_string = "BPI"
-
-
-    chart_path.title_string = f"Premium Breakdown - {chart_path.uw_adj_basis} - {chart_path.premium_basis}"
--- hyperexponential.rater.bbt-main/source_files/6371_v1.2.19/data_schema/sch_pricing.py
+++
@@ -1,8 +0,0 @@
-import hx_data_schema as hx
-from data_schema.sch_utilities import thousands_format, percent_format, integer_format
-
-def sch_pricing(cds):
-    cds.extend_node_rater_defined("cds", {
-    })
-
-
